fcd1d726b721fb34beab06c13b2d9bd868f01ff1
etheriannn <rianziwalker@gmail.com>
2025-03-04 14:57:46 +0700

    Add buyer and inspector modifiers, and implement deposit and inspection status functions in Escrow contract

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -21,10 +21,21 @@ contract Escrow {
         _;
     }
 
+    modifier onlyBuyer(uint256 _nftID) {
+        require(msg.sender == buyer[_nftID], "Only buyer can call this method");
+        _;
+    }
+
+    modifier onlyInspector() {
+        require(msg.sender == inspector, "Only inspector can call this method");
+        _;
+    }
+
     mapping(uint256 => bool) public isListed;
     mapping(uint256 => uint256) public purchasePrice;
     mapping(uint256 => uint256) public escrowAmount;
     mapping(uint256 => address) public buyer;
+    mapping(uint256 => bool) public inspectionPassed;
 
     constructor(
         address _nftAddress, 
@@ -54,6 +65,27 @@ contract Escrow {
         purchasePrice[_nftID] = _purchasePrice;
         escrowAmount[_nftID] = _escrowAmount;
         buyer[_nftID] = _buyer;
+    }
+
+    // Put under contract (only buyer - payable escrow)
+    function depositEarnest(uint256 _nftID) public payable onlyBuyer(_nftID) {
+        require(msg.value >= escrowAmount[_nftID]);
+    }
+
+    // Update Inspection Status (only inspector)
+    function updateInspectionStatus(uint256 _nftID, bool _passed) 
+    public onlyInspector {
+        inspectionPassed[_nftID] = _passed;
+    }
+
+    // function to receive ether in contract 
+    receive() external payable {
+
+    }
+
+    // function to get balance of contract
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 
 }
