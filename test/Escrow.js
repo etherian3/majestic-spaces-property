ea2198d94473c7f2d54ec8105820cfc5c7ef5dd7
etheriannn <rianziwalker@gmail.com>
2025-03-04 13:22:39 +0700

    Setup Escrow test to save addresses and deploy RealEstate contract

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,10 +6,30 @@ const tokens = (n) => {
 };
 
 describe("Escrow", () => {
-  it("saves the addresses", async () => {
-    const RealEstate = await ethers.getContractFactory("RealEstate");
-    const realEstate = await RealEstate.deploy();
+  let buyer, seller, inspector, lender;
+  let realEstate, escrow;
 
-    console.log(realEstate.address);
+  it("saves the addresses", async () => {
+    // Setup accounts hardhat
+    [buyer, seller, inspector, lender] = await ethers.getSigners();
+
+    const RealEstate = await ethers.getContractFactory("RealEstate");
+    realEstate = await RealEstate.deploy();
+
+    // Mint
+    let transaction = await realEstate
+      .connect(seller)
+      .mint(
+        "https://ipfs.io/ipfs/bafybeih54wmlis4jzkk65kaetmwnd6na2p7qkq3gmawnnavkrzb2uwnla4/1.json"
+      );
+    await transaction.wait();
+
+    const Escrow = await ethers.getContractFactory("Escrow");
+    escrow = await Escrow.deploy(
+      realEstate.address,
+      seller.address,
+      inspector.address,
+      lender.address
+    );
   });
 });
